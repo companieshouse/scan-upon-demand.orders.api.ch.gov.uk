ef1a0d499dff19ab5d727408d2bd3d7fd0a42182
rjadhav <rjadhav@companieshouse.gov.uk>
2020-09-17 08:13:40 +0100

    Replace tabs with spaces

--- src/main/java/uk/gov/companieshouse/scanupondemand/orders/api/service/ScanUponDemandItemService.java
+++ src/main/java/uk/gov/companieshouse/scanupondemand/orders/api/service/ScanUponDemandItemService.java
@@ -13,52 +13,52 @@ import java.util.Optional;
 @Service
 public class ScanUponDemandItemService {
 
-	private final ScanUponDemandItemRepository repository;
-	private final IdGeneratorService idGenerator;
-	private final EtagGeneratorService etagGenerator;
-	private final LinksGeneratorService linksGenerator;
+    private final ScanUponDemandItemRepository repository;
+    private final IdGeneratorService idGenerator;
+    private final EtagGeneratorService etagGenerator;
+    private final LinksGeneratorService linksGenerator;
 
-	public ScanUponDemandItemService(final ScanUponDemandItemRepository repository,
+    public ScanUponDemandItemService(final ScanUponDemandItemRepository repository,
 
-			final IdGeneratorService idGenerator, final EtagGeneratorService etagGenerator,
-			final LinksGeneratorService linksGenerator) {
-		this.repository = repository;
-		this.idGenerator = idGenerator;
-		this.etagGenerator = etagGenerator;
-		this.linksGenerator = linksGenerator;
-	}
+                                     final IdGeneratorService idGenerator, final EtagGeneratorService etagGenerator,
+                                     final LinksGeneratorService linksGenerator) {
+        this.repository = repository;
+        this.idGenerator = idGenerator;
+        this.etagGenerator = etagGenerator;
+        this.linksGenerator = linksGenerator;
+    }
 
-	/**
-	 * Creates the scan upon demand item in the database.
-	 *
-	 * @param item the item to be created
-	 * @return the created item
-	 */
-	public ScanUponDemandItem createScanUponDemandItem(final ScanUponDemandItem item) {
-		item.setId(idGenerator.autoGenerateId());
-		setCreationDateTimes(item);
-		item.setEtag(etagGenerator.generateEtag());
-		item.setLinks(linksGenerator.generateLinks(item.getId()));
-		return repository.save(item);
-	}
+    /**
+     * Creates the scan upon demand item in the database.
+     *
+     * @param item the item to be created
+     * @return the created item
+     */
+    public ScanUponDemandItem createScanUponDemandItem(final ScanUponDemandItem item) {
+        item.setId(idGenerator.autoGenerateId());
+        setCreationDateTimes(item);
+        item.setEtag(etagGenerator.generateEtag());
+        item.setLinks(linksGenerator.generateLinks(item.getId()));
+        return repository.save(item);
+    }
 
-	/**
-	 * Sets the created at and updated at date time 'timestamps' to now.
-	 *
-	 * @param item the item to be 'timestamped'
-	 */
-	void setCreationDateTimes(final ScanUponDemandItem item) {
-		final LocalDateTime now = LocalDateTime.now();
-		item.setCreatedAt(now);
-		item.setUpdatedAt(now);
-	}
+    /**
+     * Sets the created at and updated at date time 'timestamps' to now.
+     *
+     * @param item the item to be 'timestamped'
+     */
+    void setCreationDateTimes(final ScanUponDemandItem item) {
+        final LocalDateTime now = LocalDateTime.now();
+        item.setCreatedAt(now);
+        item.setUpdatedAt(now);
+    }
 
-	/**
-	 * Returns scan upon demand item
-	 * @param id id of the requested item
-	 * @return
-	 */
-	public Optional<ScanUponDemandItem> getScanUponDemandItemById(String id) {
-		return repository.findById(id);
-	}
+    /**
+     * Returns scan upon demand item
+     * @param id id of the requested item
+     * @return
+     */
+    public Optional<ScanUponDemandItem> getScanUponDemandItemById(String id) {
+        return repository.findById(id);
+    }
 }
