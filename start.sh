af79eb273f6817d8e688ef020de4a583a9e5eb34
caismith-ch <csmith2@companieshouse.gov.uk>
2020-10-02 08:09:01 +0100

    Fix broken tests causing failed test job in concourse

--- start.sh
+++ start.sh
@@ -27,5 +27,6 @@ else
     source "${APP_DIR}/app_env"
 fi
 
-exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" "${APP_DIR}/missing-image-delivery.orders.api.ch.gov.uk.jar"
+#exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" "${APP_DIR}/missing-image-delivery.orders.api.ch.gov.uk.jar"
+exec java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=21095 -jar -Dserver.port="${PORT}" "${APP_DIR}/missing-image-delivery.orders.api.ch.gov.uk.jar"
 
