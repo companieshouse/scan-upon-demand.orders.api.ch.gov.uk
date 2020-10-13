b996ad230cebe6d92b7d59d1448247c6c55291dc
rjadhav <rjadhav@companieshouse.gov.uk>
2020-10-13 12:29:37 +0100

    Refactor enum to use constructor initialisation

--- src/main/java/uk/gov/companieshouse/missingimagedelivery/orders/api/model/FilingHistoryCategory.java
+++ src/main/java/uk/gov/companieshouse/missingimagedelivery/orders/api/model/FilingHistoryCategory.java
@@ -3,186 +3,42 @@ package uk.gov.companieshouse.missingimagedelivery.orders.api.model;
 import uk.gov.companieshouse.missingimagedelivery.orders.api.converter.EnumValueNameConverter;
 
 public enum FilingHistoryCategory {
-    ACCOUNTS() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_ACCOUNTS;
-        }
-    },
-    ANNUAL_RETURN() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN;
-        }
-    },
-    CONFIRMATION_STATEMENT() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN;
-        }
-    },
-    RETURN() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN;
-        }
-    },
-    OFFICERS() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_APPOINTMENT;
-        }
-    },
-    OFFICER() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_APPOINTMENT;
-        }
-    },
-    ADDRESS() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_REGISTERED_OFFICE;
-        }
-    },
-    MORTGAGE() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MORTGAGE;
-        }
-    },
-    INSOLVENCY() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_LIQUIDATION;
-        }
-    },
-    LIQUIDATION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_LIQUIDATION;
-        }
-    },
-    INCORPORATION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_INCORPORATION;
-        }
-    },
-    CHANGE_OF_NAME() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_CHANGE_OF_NAME;
-        }
-    },
-    CAPITAL() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_CAPITAL;
-        }
-    },
-    RESOLUTION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    MISCELLANEOUS() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    AUDITORS() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    GAZETTE() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    OTHER() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    CERTIFICATE() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    DISSOLUTION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    REREGISTRATION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    HISTORICAL() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    RESTORATION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    COURT_ORDER() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    SOCIAL_LANDLORD() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    CHANGE_OF_CONSTITUTION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    DOCUMENT_REPLACEMENT() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    PERSONS_WITH_SIGNIFICANT_CONTROL() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    ANNOTATION() {
-        @Override
-        public ProductType getProductType() {
-            return ProductType.MISSING_IMAGE_DELIVERY_MISC;
-        }
-    },
-    UNHANDLED_CATEGORY() {
-        @Override
-        public ProductType getProductType() {
-            return null;
-        }
-    };
+    ACCOUNTS(ProductType.MISSING_IMAGE_DELIVERY_ACCOUNTS),
+    ANNUAL_RETURN(ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN),
+    CONFIRMATION_STATEMENT(ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN),
+    RETURN(ProductType.MISSING_IMAGE_DELIVERY_ANNUAL_RETURN),
+    OFFICERS(ProductType.MISSING_IMAGE_DELIVERY_APPOINTMENT),
+    OFFICER(ProductType.MISSING_IMAGE_DELIVERY_APPOINTMENT),
+    ADDRESS(ProductType.MISSING_IMAGE_DELIVERY_REGISTERED_OFFICE),
+    MORTGAGE(ProductType.MISSING_IMAGE_DELIVERY_MORTGAGE),
+    INSOLVENCY(ProductType.MISSING_IMAGE_DELIVERY_LIQUIDATION),
+    LIQUIDATION(ProductType.MISSING_IMAGE_DELIVERY_LIQUIDATION),
+    INCORPORATION(ProductType.MISSING_IMAGE_DELIVERY_INCORPORATION),
+    CHANGE_OF_NAME(ProductType.MISSING_IMAGE_DELIVERY_CHANGE_OF_NAME),
+    CAPITAL(ProductType.MISSING_IMAGE_DELIVERY_CAPITAL),
+    RESOLUTION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    MISCELLANEOUS(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    AUDITORS(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    GAZETTE(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    OTHER(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    CERTIFICATE(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    DISSOLUTION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    REREGISTRATION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    HISTORICAL(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    RESTORATION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    COURT_ORDER(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    SOCIAL_LANDLORD(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    CHANGE_OF_CONSTITUTION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    DOCUMENT_REPLACEMENT(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    PERSONS_WITH_SIGNIFICANT_CONTROL(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    ANNOTATION(ProductType.MISSING_IMAGE_DELIVERY_MISC),
+    UNHANDLED_CATEGORY(null);
+
+    private ProductType productType;
+
+    private FilingHistoryCategory(ProductType productType) {
+        this.productType = productType;
+    }
 
     public static FilingHistoryCategory enumValueOf(String v) {
         try {
@@ -198,6 +54,6 @@ public enum FilingHistoryCategory {
     }
 
     public ProductType getProductType(){
-        return null;
+        return this.productType;
     }
 }
